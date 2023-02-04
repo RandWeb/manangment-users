1dab9d9aca76611912ffbfc04a3f432ddb102d50
مهرداد تابش <mehrdadit12@gmail.com>
2023-02-04 12:03:15 -0800

    اضافه کردن  اطلاعات کاربر با ایدی کاربر در روت

--- src/components/users/user/User.js
+++ src/components/users/user/User.js
@@ -1,11 +1,32 @@
-import { Component } from "react";
-import { useParams } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
-const User = (props) => {
-    const params = useParams();
+const User = () => {
+  const [user, setUser] = useState({});
+  const params = useParams();
 
-  return <h1>User{console.log(params.id)}</h1>;
+  useEffect(async () => {
+    const response = await axios.get(
+      `https://reqres.in/api/users/${params.id}`
+    );
+    console.log(response.data.data);
+    setUser(response.data.data);
+  },[]);
+
+  return (
+    <>
+      <img
+        src={user.avatar}
+        style={{ borderRadius: "50%", width: "100px" }}
+        alt=""
+      />
+      <a>
+        {user.first_name} {user.last_name}
+      </a>
+      <h5>{user.email}</h5>
+    </>
+  );
 };
 
 export default User;
-
