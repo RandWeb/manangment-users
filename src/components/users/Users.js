7d30c956c527ca6638df12243bf8717b05f04c82
مهرداد تابش <mehrdadit12@gmail.com>
2023-02-04 07:14:21 -0800

    استفاده از AXIONS برای  عملایات CRUD

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import axios from "axios";
+import LoandingUsers from "../loading/loadingUsers";
 
 class Users extends Component {
   state = {
     users: [],
+    isLoading: true,
   };
   async componentDidMount() {
     const response = await axios.get("https://reqres.in/api/users");
+    this.setState({ users: response.data.data, isLoading: false });
   }
   render() {
     return (
@@ -15,47 +18,90 @@ class Users extends Component {
           create
         </button>
         <div className="row">
-          {this.state.users.map((user) => {
-            return (
-              <div className="col-4 text-center p-5">
-                <img
-                  src={user.avatar}
-                  style={{ borderRadius: "50%", width: "100px" }}
-                  alt=""
-                />
-                <h4>
-                  {user.first_name} {user.last_name}
-                </h4>
-                <h5>{user.email}</h5>
-                <div className="row">
-                  <div className="col-6">
-                    <button
-                      onClick={this.handleUpdate}
-                      className="btn btn-info btn-sm"
-                    >
-                      update
-                    </button>
+          {this.state.isLoading ? (
+            <LoandingUsers />
+          ) : (
+            <>
+              {this.state.users.map((user, index) => {
+                return (
+                  <div className="col-4 text-center p-5" key={index}>
+                    <img
+                      src={user.avatar}
+                      style={{ borderRadius: "50%", width: "100px" }}
+                      alt=""
+                    />
+                    <h4>
+                      {user.first_name} {user.last_name}
+                    </h4>
+                    <h5>{user.email}</h5>
+                    <div className="row">
+                      <div className="col-6">
+                        <button
+                          onClick={() => {
+                            this.handleUpdate(user);
+                          }}
+                          className="btn btn-info btn-sm"
+                        >
+                          update
+                        </button>
+                      </div>
+                      <div className="col-6">
+                        <button
+                          onClick={() => {
+                            this.handleDelete(user);
+                          }}
+                          className="btn btn-danger btn-sm"
+                        >
+                          delete
+                        </button>
+                      </div>
+                    </div>
                   </div>
-                  <div className="col-6">
-                    <button
-                      onClick={this.handleDelete}
-                      className="btn btn-danger btn-sm"
-                    >
-                      delete
-                    </button>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
+                );
+              })}
+            </>
+          )}
         </div>
       </>
     );
   }
 
-  handleCreate = () => {};
-  handleUpdate = () => {};
-  handleDelete = () => {};
+  handleCreate = async (user) => {
+    const newUser = {
+      first_name: "firstName",
+      last_name: "lastName",
+      email: "email@email.com",
+      avatar:
+        "https://cdn.gerdoo.me/proxy/?key=05a2a9652891800c615a6596d48fd43da098d4cd064a0acb610074b4719225d2&url=https%3A%2F%2Fencrypted-tbn0.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcRQsu34yqIKdjK5cAWEcuUq3ryD30iiqd2ArQ%26usqp%3DCAU",
+    };
+
+    const reponse = await axios.post("https://reqres.in/api/users", newUser);
+
+    console.log(reponse);
+
+    this.setState({ users: [newUser, ...this.state.users] });
+  };
+  handleUpdate = async (user) => {
+    user.first_name = "new name";
+
+    const response = await axios.put(
+      `https://reqres.in/api/users/${user.id}`,
+      user
+    );
+    console.log(response);
+
+    const updateUsers = [...this.state.users];
+    const index = updateUsers.indexOf(user);
+    updateUsers[index] = { ...user };
+
+    this.setState({ users: updateUsers });
+  };
+  handleDelete = async (user) => {
+    const response = await axios.delete(`https://reqres.in/api/users/${user.id}`);
+    console.log(response);
+    const newUsers = this.state.users.filter(u=>u.id !== user.id);
+    this.setState({ users: newUsers });
+  };
 }
 
 export default Users;
